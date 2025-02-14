e3863919d0c276f5063520a92d80a78881b619fd
AFanOfHololive <ios2004@icloud.com>
2025-02-14 21:32:07 +0700

    chore: minor changes to the pipe config

--- asm/api/ai_model.py
+++ asm/api/ai_model.py
@@ -1,4 +1,8 @@
 from transformers import pipeline
 model_path = "./model"
 
-pipe = pipeline("text2text-generation", model = model_path)
+pipe = pipeline(
+        "text2text-generation",
+        model = model_path,
+        max_length=100
+        )
